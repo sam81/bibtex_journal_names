b94c61be7d613e31dea81e5ed87a5409c3f88a27
Samuele Carcagno <sam.carcagno@gmail.com>
2020-01-20 22:30:03 +0100

    Initial commit

---
+++ mkdbs.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+mkdir -p ../bibtex_database
+
+cat subs_long.txt ../bibtex_database/A.bib ../bibtex_database/B.bib ../bibtex_database/C.bib ../bibtex_database/D.bib ../bibtex_database/E.bib ../bibtex_database/F.bib ../bibtex_database/G.bib ../bibtex_database/H.bib ../bibtex_database/I.bib ../bibtex_database/J.bib ../bibtex_database/K.bib ../bibtex_database/L.bib ../bibtex_database/M.bib ../bibtex_database/N.bib ../bibtex_database/O.bib ../bibtex_database/P.bib ../bibtex_database/Q.bib ../bibtex_database/R.bib ../bibtex_database/S.bib ../bibtex_database/T.bib ../bibtex_database/U.bib ../bibtex_database/V.bib ../bibtex_database/W.bib ../bibtex_database/X.bib ../bibtex_database/Y.bib ../bibtex_database/Z.bib > ../bibtex_database/refs_long.bib
+
+cat subs_short.txt ../bibtex_database/A.bib ../bibtex_database/B.bib ../bibtex_database/C.bib ../bibtex_database/D.bib ../bibtex_database/E.bib ../bibtex_database/F.bib ../bibtex_database/G.bib ../bibtex_database/H.bib ../bibtex_database/I.bib ../bibtex_database/J.bib ../bibtex_database/K.bib ../bibtex_database/L.bib ../bibtex_database/M.bib ../bibtex_database/N.bib ../bibtex_database/O.bib ../bibtex_database/P.bib ../bibtex_database/Q.bib ../bibtex_database/R.bib ../bibtex_database/S.bib ../bibtex_database/T.bib ../bibtex_database/U.bib ../bibtex_database/V.bib ../bibtex_database/W.bib ../bibtex_database/X.bib ../bibtex_database/Y.bib ../bibtex_database/Z.bib > ../bibtex_database/refs_short.bib
+
